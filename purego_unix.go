7c2cec76102c19c5e9b957d03ac759188df64871
Milan Nikolic <gen2brain@gmail.com>
2024-11-08 11:52:13 +0100

    Check elf for dynamic symbols

--- purego_unix.go
+++ purego_unix.go
@@ -3,7 +3,10 @@
 package avif
 
 import (
+	"debug/elf"
 	"fmt"
+	"os"
+	"runtime"
 
 	"github.com/ebitengine/purego"
 )
@@ -13,10 +16,35 @@ const (
 )
 
 func loadLibrary() (uintptr, error) {
+	if runtime.GOOS == "linux" && !isDynamicBinary() {
+		return 0, fmt.Errorf("not a dynamic binary")
+	}
+
 	handle, err := purego.Dlopen(libname, purego.RTLD_NOW|purego.RTLD_GLOBAL)
 	if err != nil {
 		return 0, fmt.Errorf("cannot load library: %w", err)
 	}
 
-	return uintptr(handle), nil
+	return handle, nil
+}
+
+func isDynamicBinary() bool {
+	fileName, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+
+	fl, err := elf.Open(fileName)
+	if err != nil {
+		panic(err)
+	}
+
+	defer fl.Close()
+
+	_, err = fl.DynamicSymbols()
+	if err == nil {
+		return true
+	}
+
+	return false
 }
